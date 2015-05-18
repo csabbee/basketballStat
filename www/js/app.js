5cc01e32ea5fa54536de9cee0fef90755f53e587
Csaba Valyi <csaba_valyi@epam.com>
2015-05-18 16:48:40 +0200

    app renamed

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova'])
+angular.module('basketballStat', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform, $cordovaSplashscreen) {
   $ionicPlatform.ready(function() {
     setTimeout(function() {
-      $$cordovaSplashscreen.hide();
+      $cordovaSplashscreen.hide();
     }, 5000);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
