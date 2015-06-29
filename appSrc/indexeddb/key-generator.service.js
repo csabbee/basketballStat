1b2472793804b108119ec25d2a0089fdc309f940
Csaba Valyi <csaba.valyi1@gmail.com>
2015-06-29 21:25:24 +0200

    unique key generation for multiple object store

--- appSrc/indexeddb/key-generator.service.js
+++ appSrc/indexeddb/key-generator.service.js
@@ -1,16 +1,40 @@
 angular.module('basketballStat.storage')
     .service('KeyGenerator', function() {
-        var seed = 1;
+        var generators = [];
+
         return {
             setSeed: setSeed,
             nextKey: nextKey
         };
 
-        function setSeed(number) {
-            seed = number > 0 ? number : seed;
+        /**
+         * Set the generator seed
+         * @param {Object} obj
+         * @param {string} obj.store
+         * @param {number[]} obj.usedIds
+         */
+        function setSeed(obj) {
+            var generatorObj = _.extend(obj, {maxId: _.max(obj.usedIds)});
+
+            if (generators.indexOf(generatorObj) !== -1) {
+                var generatorIndex = generators.indexOf(generatorObj);
+                generatorObj[generatorIndex] = generatorObj;
+            } else {
+                generators.push(generatorObj);
+            }
         }
 
-        function nextKey() {
-            return seed++;
+        /**
+         * Returns the next available key to the given object store
+         * @param {string} store
+         * @returns {number}
+         */
+        function nextKey(store) {
+            var generatorObj = _.find(generators, generator => generator.store === store),
+                nextKey = _.first(_.difference(_.range(generatorObj.maxId), generatorObj.usedIds));
+            nextKey = !_.isUndefined(nextKey) ? nextKey : ++generatorObj.maxId;
+            generatorObj.usedIds.push(nextKey);
+
+            return nextKey;
         }
     });
