7ea9d732ecfdb7b764f9389958b042a3433bd046
Csaba Valyi <csaba.valyi1@gmail.com>
2015-07-05 11:07:23 +0200

    disabling IE and Chrome for test running

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,11 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome', 'PhantomJS', 'IE'],
+        browsers: [
+            //'Chrome',
+            //'IE',
+            'PhantomJS'
+        ],
 
 
         // Continuous Integration mode
