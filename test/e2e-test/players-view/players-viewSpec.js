726fc458b022fef7cda7db9cc2ba534b7d562b25
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-02 08:48:20 +0200

    clicking on Add Player button

--- test/e2e-test/players-view/players-viewSpec.js
+++ test/e2e-test/players-view/players-viewSpec.js
@@ -5,4 +5,13 @@ describe('basketballstat players view', function() {
 
     expect(browser.getTitle()).toEqual('Players View');
   });
+
+  it('should have title called New Player', function() {
+    browser.get('http://localhost:8080/#/app/home');
+    browser.setLocation('app/players');
+
+    element(by.css('button[ui-sref="app.players.newPlayer"]')).click();
+
+    expect(browser.getCurrentUrl()).toBe('http://localhost:8080/#/app/players/new');
+  });
 });
