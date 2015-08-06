7101d0329a92543d3ab24733f462432fcd82734f
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-06 14:54:59 +0200

    removed unnecessary test case

--- test/e2e-test/players-view/players-viewSpec.js
+++ test/e2e-test/players-view/players-viewSpec.js
@@ -2,20 +2,11 @@ var PlayersPage = require('./players-viewPO.js');
 
 describe('basketballstat players view', () => {
     var playersPage = new PlayersPage(),
-        playersView = 'Players View',
-        newPlayerUrl = 'http://localhost:8080/#/app/players/new';
+        playersView = 'Players View';
     
     it(`should have title called ${playersView}`, () => {
         playersPage.get();
 
         expect(browser.getTitle()).toEqual(playersView);
-    });
-
-    it(`should have the url: ${newPlayerUrl}`, () => {
-        playersPage.get();
-
-        element(by.css('button[ui-sref="app.players.newPlayer"]')).click();
-
-        expect(browser.getCurrentUrl()).toBe(newPlayerUrl);
     });
 });
