d32ac87e94ba2bbfd8d00742511af8b70b708373
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-06 14:54:39 +0200

    method for getting players list

--- test/e2e-test/players-view/players-viewPO.js
+++ test/e2e-test/players-view/players-viewPO.js
@@ -1,11 +1,18 @@
 module.exports = function() {
-    
+    var playersList;
+
     return {
-        get: get
-    }
+        get: get,
+        getPlayers: getPlayers
+    };
 
     function get() {
         browser.get('http://localhost:8080/#/app/home');
         browser.setLocation('app/players');
+        playersList = element.all(by.repeater('player in PlayersController.players'));
+    }
+
+    function getPlayers() {
+        return element.all(by.repeater('player in PlayersController.players'));
     }
 }
