6db795e3950f5d61dd261af7e1b5f0bb89e3262c
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-06 13:26:33 +0200

    using PlayersPage page object in test spec

--- test/e2e-test/players-view/players-viewSpec.js
+++ test/e2e-test/players-view/players-viewSpec.js
@@ -1,17 +1,18 @@
+var PlayersPage = require('./players-viewPO.js');
+
 describe('basketballstat players view', () => {
-    var playerView = 'Player View',
+    var playersPage = new PlayersPage(),
+        playersView = 'Players View',
         newPlayerUrl = 'http://localhost:8080/#/app/players/new';
     
-    it(`should have title called ${playerView}`, () => {
-        browser.get('http://localhost:8080/#/app/home');
-        browser.setLocation('app/players');
+    it(`should have title called ${playersView}`, () => {
+        playersPage.get();
 
-        expect(browser.getTitle()).toEqual(playerView);
+        expect(browser.getTitle()).toEqual(playersView);
     });
 
     it(`should have the url: ${newPlayerUrl}`, () => {
-        browser.get('http://localhost:8080/#/app/home');
-        browser.setLocation('app/players');
+        playersPage.get();
 
         element(by.css('button[ui-sref="app.players.newPlayer"]')).click();
 
