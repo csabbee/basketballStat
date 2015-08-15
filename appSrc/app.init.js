fb9e39549ae1c7d43294fa5dc66faa04a2ac6637
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-15 11:23:44 +0200

    players related route configs moved to players module

--- appSrc/app.init.js
+++ appSrc/app.init.js
@@ -31,38 +31,6 @@ angular.module('basketballStat.mainModule', [
 				}
 			}
 		})
-		.state('app.players', {
-			url: '/players',
-			views: {
-				'menuContent': {
-					templateUrl: 'players/players.html',
-					controller: 'PlayersController as PlayersController',
-					resolve: {
-						players: ['IndexedDbService', function(IndexedDbService) {
-							return IndexedDbService.getAllPlayer();
-						}]
-					}
-				}
-			}
-		})
-		.state('app.players.player', {
-			url: '/:ssnId',
-			views: {
-				'player': {
-					templateUrl: 'players/player/player.html',
-					controller: 'PlayerController as PlayerController'
-				}
-			}
-		})
-		.state('app.players.newPlayer', {
-			url: '/new',
-			views: {
-				'player': {
-					templateUrl: 'players/new-player/new-player.html',
-					controller: 'NewPlayerController as NewPlayerController'
-				}
-			}
-		})
 }).config(['$provide', function ($provide) {
 	'use strict';
 	$provide.decorator('$rootScope', ['$delegate', function ($delegate) {
