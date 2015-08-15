ccc7da0e9196282d9aa403ea28cefe35fed64809
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-15 19:42:08 +0200

    MatchesDbService added as dependency to NewMatchController

--- appSrc/matches/new-match/new-match.controller.js
+++ appSrc/matches/new-match/new-match.controller.js
@@ -1,4 +1,4 @@
 angular.module('basketballStat.matches')
-    .controller('NewMatchController', ()=>{
-        console.log('new match');
+    .controller('NewMatchController', (MatchesDbService)=>{
+
     });
