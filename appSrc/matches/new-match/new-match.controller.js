57d4d2446a3239153c977aa2240f5fbd4f1c5323
Csaba Valyi <csaba.valyi1@gmail.com>
2015-08-23 19:02:39 +0200

    saving created match

--- appSrc/matches/new-match/new-match.controller.js
+++ appSrc/matches/new-match/new-match.controller.js
@@ -1,8 +1,26 @@
 angular.module('basketballStat.matches')
-    .controller('NewMatchController', function(MatchesDbService, players) {
+    .controller('NewMatchController', function(MatchesDbService, players, $scope, $cordovaToast, StateHandler) {
         var vm = this,
             timeStamp = new Date();
         console.log(`new match at: ${timeStamp.toDateString()}`);
 
+        vm.save = function(match, form) {
+            var playing = _.filter(players, _.partial(idPresent, _.keys(match.playerIds)));
+
+            if (form.$dirty && form.$valid) {
+                if (playing.length >= 5) {
+                    match.time = timeStamp;
+                    MatchesDbService.addMatch(match);
+                    StateHandler.goBack();
+                } else {
+                    $cordovaToast.showLongCenter('Need to select least 5 player');
+                }
+            }
+        };
+
+        function idPresent(playerIds, player) {
+            return _.contains(playerIds, ''+player.ssnId);
+        }
         vm.players = players;
+        $scope.match = {};
     });
