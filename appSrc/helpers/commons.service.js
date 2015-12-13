98fbd55fffb9c3e60f9c31c544ba92944f641ad8
Csaba Valyi <csaba.valyi1@gmail.com>
2015-12-13 21:22:02 +0100

    implementing Commons.swapArrayItemsByIndex

--- appSrc/helpers/commons.service.js
+++ appSrc/helpers/commons.service.js
@@ -1,10 +1,11 @@
 angular.module('basketballStat.helpers')
 	.service('Commons', Commons);
 
-function Commons($q) {
+function Commons() {
 	return {
 		pluckDoc: pluckDoc,
-		logToConsole: logToConsole
+		logToConsole: logToConsole,
+        swapArrayItemsByIndex: swapArrayItemsByIndex
 	};
 
 	function pluckDoc(documents) {
@@ -15,4 +16,21 @@ function Commons($q) {
 	function logToConsole(message) {
 		return console.log(message);
 	}
+
+	function swapArrayItemsByIndex(fromIndex, toIndex, array) {
+        if (fromIndex === toIndex) {
+            return Promise.resolve(array);
+        }
+
+        var lowerIndex = fromIndex < toIndex ? fromIndex : toIndex;
+        var higherIndex = lowerIndex === fromIndex ? toIndex : fromIndex;
+        var firstPart = array.slice(0, lowerIndex);
+        var middlePart = array.slice(lowerIndex, higherIndex);
+        var endPart = array.slice(higherIndex);
+        var lowerElement = middlePart.shift();
+        var higherElement = endPart.shift();
+        middlePart.unshift(higherElement);
+        endPart.unshift(lowerElement);
+        return Promise.resolve(firstPart.concat(middlePart, endPart));
+    }
 }
