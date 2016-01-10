e7edac94dcfab2d7f0533ecde0aebae8c2db59be
Csaba Valyi <csaba.valyi1@gmail.com>
2016-01-10 21:24:24 +0100

    stat event handler remove stat created

--- appSrc/matches/match/stats/stat-event-handler.service.js
+++ appSrc/matches/match/stats/stat-event-handler.service.js
@@ -2,19 +2,33 @@ angular.module('basketballStat.matches')
     .service('StatEventHandler', StatEventHandler);
 
 
-function StatEventHandler() {
+function StatEventHandler($q) {
+    const TWO_POINT_MADE = '2pt made',
+        THREE_POINT_MADE = '3pt made',
+        FREE_THROW_MADE = 'FT made',
+        TWO_POINT_ATTEMPT = '2pt attempt',
+        THREE_POINT_ATTEMPT = '3pt attempt',
+        FREE_THROW_ATTEMPT = 'FT attempt';
+
+    const SHOT_VALUES = {};
+    SHOT_VALUES[TWO_POINT_MADE] = 2;
+    SHOT_VALUES[THREE_POINT_MADE] = 3;
+    SHOT_VALUES[FREE_THROW_MADE] = 1;
+
+    const SHOT_ATTEMPT = [THREE_POINT_ATTEMPT, TWO_POINT_ATTEMPT, FREE_THROW_ATTEMPT];
+
     var playerStatTemplate = {
         twoPoint: {
-            made: '2pt made',
-            attempt: '2pt attempt'
+            made: TWO_POINT_MADE,
+            attempt: TWO_POINT_ATTEMPT
         },
         threePoint: {
-            made: '3pt made',
-            attempt: '3pt attempt'
+            made: THREE_POINT_MADE,
+            attempt: THREE_POINT_ATTEMPT
         },
         freeThrow: {
-            made: 'FT made',
-            attempt: 'FT attempt'
+            made: FREE_THROW_MADE,
+            attempt: FREE_THROW_ATTEMPT
         },
         rebound: {
             offensive: 'Offensive Rebound',
@@ -37,11 +51,54 @@ function StatEventHandler() {
         var [stats, stat, key] = args;
         var event = {};
         event.name = !key ? playerStatTemplate[stat] : playerStatTemplate[stat][key];
+        event.statKey = !key ? `${stat}` : `${stat}.${key}`;
+        event.removeStat = createStatRemoveFunction(stats);
+
+
+        function createStatRemoveFunction(stats) {
+            return function() {
+                var {name: statName, statKey} = event,
+                    [stat, key] = statKey.split('.');
+
+                if (!key) {
+                    stats[stat] = stats[stat] - 1;
+                } else if (!isShotAttempt()) {
+                    stats[stat][key] = stats[stat][key] - 1;
+
+                    if (isShotMade()) {
+                        stats[stat]['attempt'] = stats[stat]['attempt'] - 1;
+                    }
+
+                } else if (isShotAttemptAndHavingMoreAttemptThanMade()){
+                    stats[stat][key] = stats[stat][key] - 1;
+                } else {
+                    return false;
+                }
+
+                if (isShotMade()) {
+                    stats.points = stats.points - SHOT_VALUES[statName];
+                }
+
+                function isShotMade() {
+                    return SHOT_VALUES[statName];
+                }
+
+                function isShotAttempt() {
+                    return SHOT_ATTEMPT.indexOf(statName) !== -1;
+                }
+
+                function isShotAttemptAndHavingMoreAttemptThanMade() {
+                    return key === 'attempt' && stats[stat][key] > stats[stat]['made'];
+                }
+
+                return true;
+            };
+        }
   
         return event;
     }
 
     function handleEventRemove(event) {
-		
+		event.removeStat();
     }
 }
--- test/unit-test/matches/match/stat/stat-event-handler.serviceSpec.js
+++ test/unit-test/matches/match/stat/stat-event-handler.serviceSpec.js
@@ -1,4 +1,15 @@
 describe('StatEventHandler', () => {
+    const TURNOVER = 'turnover',
+        ASSIST = 'assist',
+        STEAL = 'steal',
+        BLOCK = 'block',
+        PERSONAL_FOUL = 'personalFoul',
+        REBOUND = 'rebound',
+        TWO_POINT = 'twoPoint',
+        THREE_POINT = 'threePoint',
+        FREE_THROW = 'freeThrow';
+    const SHOTS = [FREE_THROW, TWO_POINT, THREE_POINT];
+
 	beforeEach(() => {
 		angular.mock.module('ui.router');
 		angular.mock.module('basketballStat.storage');
@@ -7,58 +18,139 @@ describe('StatEventHandler', () => {
 	});
 	beforeEach(module('basketballStat.matches'));
 
-	var underTest;
+	var underTest,
+        playerStat = {
+        time: 0, points: 6, twoPoint: { made: 1, attempt: 1 }, threePoint: { made: 1, attempt: 1 },
+        freeThrow: { made: 1, attempt: 1 }, rebound: { offensive: 1, defensive: 1 },
+        assist: 1, turnover: 1, steal: 1, block: 1, personalFoul: 1, events: []
+    };
 
 	beforeEach(inject(StatEventHandler => {
 		underTest = StatEventHandler;
 	}));
 
+    var testCases = [
+        { input: [playerStat, TURNOVER], expect: { name: 'Turnover' } },
+        { input: [playerStat, ASSIST], expect: { name: 'Assist' } },
+        { input: [playerStat, STEAL], expect: { name: 'Steal' } },
+        { input: [playerStat, BLOCK], expect: { name: 'Block' } },
+        { input: [playerStat, PERSONAL_FOUL], expect: { name: 'Personal Foul' } },
+        { input: [playerStat, TWO_POINT, 'made'], expect: { name: '2pt made' } },
+        { input: [playerStat, TWO_POINT, 'attempt'], expect: { name: '2pt attempt' } },
+        { input: [playerStat, THREE_POINT, 'made'], expect: { name: '3pt made' } },
+        { input: [playerStat, THREE_POINT, 'attempt'], expect: { name: '3pt attempt' } },
+        { input: [playerStat, FREE_THROW, 'made'], expect: { name: 'FT made' } },
+        { input: [playerStat, FREE_THROW, 'attempt'], expect: { name: 'FT attempt' } },
+        { input: [playerStat, REBOUND, 'offensive'], expect: { name: 'Offensive Rebound' } },
+        { input: [playerStat, REBOUND, 'defensive'], expect: { name: 'Defensive Rebound' } }
+    ];
+
 	describe(`handleEventCreation`, () => {
-		var playerStat = { 
-					time: 0, points: 6, twoPoint: { made: 1, attempt: 1 }, threePoint: { made: 1, attempt: 1 },
-					freeThrow: { made: 1, attempt: 1 }, rebound: { offensive: 1, defensive: 1 },
-					assist: 1, turnover: 1, steal: 1, block: 1, personalFoul: 1, events: []
-				};
-		beforeEach(() => {	
-			playerStat = 
-				{ 
-					time: 0, points: 0, twoPoint: { made: 0, attempt: 0 }, threePoint: { made: 0, attempt: 0 },
-					freeThrow: { made: 0, attempt: 0 }, rebound: { offensive: 0, defensive: 0 },
-					assist: 0, turnover: 0, steal: 0, block: 0, personalFoul: 0, events: []
-				};
-		});
+		testCases
+			.forEach(createAndRunTestCases);
 
-		var testCases = [
-			{ input: [playerStat, 'turnover'], expect: { name: 'Turnover' } },
-			{ input: [playerStat, 'assist'], expect: { name: 'Assist' } },
-			{ input: [playerStat, 'steal'], expect: { name: 'Steal' } },
-			{ input: [playerStat, 'block'], expect: { name: 'Block' } },
-			{ input: [playerStat, 'personalFoul'], expect: { name: 'Personal Foul' } },
-			{ input: [playerStat, 'twoPoint', 'made'], expect: { name: '2pt made' } },
-			{ input: [playerStat, 'twoPoint', 'attempt'], expect: { name: '2pt attempt' } },
-			{ input: [playerStat, 'threePoint', 'made'], expect: { name: '3pt made' } },
-			{ input: [playerStat, 'threePoint', 'attempt'], expect: { name: '3pt attempt' } },
-			{ input: [playerStat, 'freeThrow', 'made'], expect: { name: 'FT made' } },
-			{ input: [playerStat, 'freeThrow', 'attempt'], expect: { name: 'FT attempt' } },
-			{ input: [playerStat, 'freeThrow', 'attempt'], expect: { name: 'FT attempt' } },
-			{ input: [playerStat, 'rebound', 'offensive'], expect: { name: 'Offensive Rebound' } },
-			{ input: [playerStat, 'rebound', 'defensive'], expect: { name: 'Defensive Rebound' } }
-		];
-
-		testCases.forEach(createAndRunReorderItemTestCase);
-
-        function createAndRunReorderItemTestCase(testCase) {
-            (function() {
-                it(`should return for stat: ${testCase.input[1]}${testCase.input[2] ? '.'+testCase.input[2] : ''} an event with property name: ${testCase.expect.name}`, () => {
+		function createAndRunTestCases(testCase) {
+			(function() {
+                var [, input1, input2] = testCase.input,
+                    // Renaming destructured object key
+                    {name: expectedName} = testCase.expect;
+				it(`should return for stat: ${input1}${input2 ? '.' + input2 : ''} an event with property name: ${expectedName}`, () => {
 					// GIVEN
 					// WHEN
-					var result = underTest.handleEventCreation.apply(null, testCase.input);
-					
+					var {name: resultName} = underTest.handleEventCreation.apply(null, testCase.input);
+
 					// THEN
-					expect(result.name).toEqual(testCase.expect.name);
+					expect(resultName).toEqual(expectedName);
 				});
+			}());
+		}
+	});
+
+    describe(`handleEventRemove`, () => {
+        testCases
+            .map(clonePlayerStat)
+            .map(createExpectedPlayerStat)
+            .forEach(createAndRunTestCases);
+
+        function createAndRunTestCases(testCase) {
+            (function() {
+                var [stats, input1, input2] = testCase.input,
+                    expectedResult = input2 ? testCase.expect.playerStat[input1][input2] : testCase.expect.playerStat[input1],
+                    expectedPoints = testCase.expect.playerStat.points;
+
+                it(`should set stat: ${input1}${input2 ? '.' + input2 : ''} to ${expectedResult}`, () => {
+                    // GIVEN
+                    var testEvent = underTest.handleEventCreation.apply(null, testCase.input);
+                    var [stat, key] = testEvent.statKey.split('.');
+
+                    // WHEN
+                    underTest.handleEventRemove(testEvent);
+
+                    // THEN
+                    var testedStat = !key ? stats[stat] : stats[stat][key],
+                        testedPoints = stats.points;
+                    expect(testedStat).toEqual(expectedResult);
+                    if (input2 === 'made') {
+                        expect(stats[stat]['attempt']).toEqual(testCase.expect.playerStat[input1]['attempt']);
+                    }
+                    expect(testedPoints).toEqual(expectedPoints);
+                });
             }());
         }
-	});
-	
+
+        function clonePlayerStat(testCase) {
+            testCase.expect.playerStat = JSON.parse(JSON.stringify(playerStat));
+            testCase.input[0] = JSON.parse(JSON.stringify(playerStat));
+            return testCase;
+        }
+
+        function createExpectedPlayerStat(testCase) {
+            _.chain([testCase])
+                .map(handleSingleStat)
+                .map(handleCompoundStat);
+
+            function isCompoundStat(testCase) {
+                return !_.isUndefined(testCase.input[2]);
+            }
+
+            function handleSingleStat(testCase) {
+                if (!isCompoundStat(testCase)) {
+                    testCase.expect.playerStat[testCase.input[1]] = 0;
+                }
+                return testCase;
+            }
+
+            function handleCompoundStat(testCase) {
+                if (isCompoundStat(testCase)) {
+                    _.chain([testCase])
+                        .map(handleRebounds)
+                        .map(handleShots)
+                }
+                return testCase;
+
+                function handleRebounds(testCase) {
+                    if (testCase.input[1] === REBOUND) {
+                        testCase.expect.playerStat[testCase.input[1]][testCase.input[2]] = 0;
+                    }
+                    return testCase;
+                }
+
+                // TODO: only testing the made shots at the moment. Figure out how should it work for attempts
+                function handleShots(testCase) {
+                    var [, stat, key] = testCase.input;
+                    if (_.contains(SHOTS, stat)) {
+                        if (key === 'made') {
+                            testCase.expect.playerStat[stat][key] = 0;
+                            testCase.expect.playerStat[stat]['attempt'] = 0;
+                            var pointsToRemove = SHOTS.indexOf(stat) + 1;
+                            testCase.expect.playerStat.points = testCase.expect.playerStat.points - pointsToRemove;
+                        }
+                    }
+                    return testCase;
+                }
+            }
+
+            return testCase;
+        }
+    });
 });
