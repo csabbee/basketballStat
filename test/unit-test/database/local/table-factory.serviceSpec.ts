fedf5ace72ef6d849736f9b84eaa627709d27cc4
Csaba Valyi <csaba.valyi1@gmail.com>
2016-03-06 17:45:57 +0100

    working on unit tests

---
+++ test/unit-test/database/local/table-factory.serviceSpec.ts
@@ -0,0 +1,20 @@
+import {TableFactory} from '../../../../app/database/local/table-factory.service';
+
+describe('TableFactory', () => {
+    var underTest: TableFactory;
+
+    beforeEach(() => {
+        underTest = new TableFactory();
+    });
+
+    afterEach(() => {
+        underTest = null;
+    });
+
+    it('should initialize a new table', () => {
+        var testTable = 'newTable';
+        underTest.getTable(testTable);
+
+        expect(underTest.tables[testTable]).toBeDefined();
+    });
+});
